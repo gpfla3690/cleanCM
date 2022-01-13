06f19deccb9f61895bc5f34273b5e1370fa734ba
gpfla3690 <gpfla3690@gmail.com>
2022-01-14 00:27:36 +0900

    Member Entity, Service, Dao 구현

---
+++ src/main/java/com/yhr/cleanCM/controller/MemberController.java
@@ -0,0 +1,13 @@
+package com.yhr.cleanCM.controller;
+
+import com.yhr.cleanCM.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+
+}
---
+++ src/main/java/com/yhr/cleanCM/service/MemberService.java
@@ -0,0 +1,20 @@
+package com.yhr.cleanCM.service;
+
+import com.yhr.cleanCM.dao.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService implements UserDetailsService {
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return memberRepository.findByLoginId(username).get();
+    }
+}
