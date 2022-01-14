eb01b9c618e31bcba94f37b30dd9df30cdfc4466
gpfla3690 <gpfla3690@gmail.com>
2022-01-15 00:13:30 +0900

    회원가입 controller및 회원가입 페이지 생성

--- src/main/java/com/yhr/cleanCM/dto/MemberSaveForm.java
+++ src/main/java/com/yhr/cleanCM/dto/MemberSaveForm.java
@@ -2,13 +2,20 @@ package com.yhr.cleanCM.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+
 @Data
 public class MemberSaveForm {
 
+    @NotBlank
     private String loginId;
+    @NotBlank
     private String loginPw;
+    @NotBlank
     private String name;
+    @NotBlank
     private String nickname;
+    @NotBlank
     private String email;
 
 }
