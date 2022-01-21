24dd73f07aab27e3c8796f17dacd6fcd3d2ca66b
gpfla3690 <gpfla3690@gmail.com>
2022-01-21 17:02:09 +0900

    게시판 리스트 구현

--- src/main/java/com/yhr/cleanCM/controller/BoardController.java
+++ src/main/java/com/yhr/cleanCM/controller/BoardController.java
@@ -1,5 +1,6 @@
 package com.yhr.cleanCM.controller;
 
+import com.yhr.cleanCM.domain.Board;
 import com.yhr.cleanCM.dto.board.BoardSaveForm;
 import com.yhr.cleanCM.service.BoardService;
 import lombok.RequiredArgsConstructor;
@@ -7,6 +8,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -29,6 +32,18 @@ public class BoardController {
         boardService.save(boardSaveForm);
 
         return "redirect:/";
+    }
+
+    // 게시판 리스트
+    @GetMapping("/boards")
+    public String showBoardList(Model model){
+
+       List<Board> boardList = boardService.findAll();
+
+       model.addAttribute("boardList", boardList);
+
+       return "usr/board/list";
+
     }
 
 }
