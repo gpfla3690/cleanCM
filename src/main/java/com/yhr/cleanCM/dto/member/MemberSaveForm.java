58b382f590d8df1a61428f8ef1d3404baf3b51a6
gpfla3690 <gpfla3690@gmail.com>
2022-02-07 21:25:47 +0900

    회원가입 에러메시지 렌더링

--- src/main/java/com/yhr/cleanCM/dto/member/MemberSaveForm.java
+++ src/main/java/com/yhr/cleanCM/dto/member/MemberSaveForm.java
@@ -7,15 +7,15 @@ import javax.validation.constraints.NotBlank;
 @Data
 public class MemberSaveForm {
 
-    @NotBlank
+    @NotBlank(message = "아이디를 입력해 주세요")
     private String loginId;
-    @NotBlank
+    @NotBlank(message = "비밀번호를 입력해 주세요")
     private String loginPw;
-    @NotBlank
+    @NotBlank(message = "이름을 입력해 주세요")
     private String name;
-    @NotBlank
+    @NotBlank(message = "닉네임을 입력해 주세요")
     private String nickname;
-    @NotBlank
+    @NotBlank(message = "이메일을 입력해 주세요")
     private String email;
 
 }
