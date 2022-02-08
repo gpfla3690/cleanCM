6a174993431d5a5c060fd0971490dd3c5627fb53
gpfla3690 <gpfla3690@gmail.com>
2022-02-08 17:04:55 +0900

    회원 정보 수정 에러메시지 렌더링

--- src/main/java/com/yhr/cleanCM/config/SecurityConfig.java
+++ src/main/java/com/yhr/cleanCM/config/SecurityConfig.java
@@ -36,7 +36,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                         .mvcMatchers(
                                 "/articles/**",
                                 "/",
-                                "/members/modify"
+                                "/members/modify/**"
                         )
                         .permitAll()
                         .mvcMatchers(
--- src/main/java/com/yhr/cleanCM/dto/member/MemberModifyForm.java
+++ src/main/java/com/yhr/cleanCM/dto/member/MemberModifyForm.java
@@ -2,34 +2,41 @@ package com.yhr.cleanCM.dto.member;
 
 import com.yhr.cleanCM.domain.Member;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
 
 @Data
+@NoArgsConstructor
 public class MemberModifyForm {
 
     private Long id;
 
     private String loginId;
 
+    @NotBlank(message = "비밀번호를 입력해 주세요")
     private String loginPw;
 
     private String name;
 
+    @NotBlank(message = "닉네임을 입력해 주세요")
     private String nickname;
 
+    @NotBlank(message = "이메일을 입력해 주세요")
     private String email;
 
-    public MemberModifyForm(Member member) {
+    public MemberModifyForm(Member findMember) {
 
-        this.id = member.getId();
+        this.id = findMember.getId();
 
-        this.loginId = member.getLoginId();
-        this.loginPw = member.getLoginPw();
+        this.loginId = findMember.getLoginId();
+        this.loginPw = findMember.getLoginPw();
 
-        this.name = member.getName();
+        this.name = findMember.getName();
 
-        this.nickname = member.getNickname();
+        this.nickname = findMember.getNickname();
 
-        this.email = member.getEmail();
+        this.email = findMember.getEmail();
 
     }
 }
