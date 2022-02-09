8d7ad97ae742e3cba81348fcf157118e233e281e
gpfla3690 <gpfla3690@gmail.com>
2022-02-09 16:57:32 +0900

    게시판 수정 에러메시지 렌더링

--- src/main/java/com/yhr/cleanCM/controller/BoardController.java
+++ src/main/java/com/yhr/cleanCM/controller/BoardController.java
@@ -85,7 +85,8 @@ public class BoardController {
 
             BoardDTO board = boardService.getBoardDetail(id);
 
-            model.addAttribute("board", new BoardModifyForm(
+            model.addAttribute("boardId", board.getId());
+            model.addAttribute("boardModifyForm", new BoardModifyForm(
                     board.getId(),
                     board.getName(),
                     board.getDetail()
@@ -98,7 +99,14 @@ public class BoardController {
     }
 
     @PostMapping("/adm/boards/modify/{id}")
-    public String doModifyBoard(@PathVariable(name = "id") Long id, BoardModifyForm boardModifyForm) {
+    public String doModifyBoard(@PathVariable(name = "id") Long id, @Validated BoardModifyForm boardModifyForm, BindingResult bindingResult, Model model) {
+
+        BoardDTO findBoard = boardService.getBoardDetail(id);
+
+        if ( bindingResult.hasErrors()) {
+            model.addAttribute("boardId", findBoard.getId());
+            return "adm/board/modify";
+        }
 
         try {
             boardService.modify(id, boardModifyForm);
@@ -106,7 +114,7 @@ public class BoardController {
             return "adm/board/modify";
         }
 
-        return "redirect:/";
+        return "redirect:/boards";
     }
 
     @GetMapping("/adm/boards/delete/{id}")
--- src/main/java/com/yhr/cleanCM/dto/board/BoardModifyForm.java
+++ src/main/java/com/yhr/cleanCM/dto/board/BoardModifyForm.java
@@ -3,6 +3,8 @@ package com.yhr.cleanCM.dto.board;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+
 
 @Data
 @AllArgsConstructor
@@ -10,6 +12,7 @@ public class BoardModifyForm {
 
     private Long id;
 
+    @NotBlank(message = "게시판 이름을 입력해 주세요")
     private String name;
 
     private String detail;
