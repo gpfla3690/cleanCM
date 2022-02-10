9337a7c36d57dc8f6f3fb5b89f5ced465b8bcb9e
gpfla3690 <gpfla3690@gmail.com>
2022-02-10 15:48:30 +0900

    회원 관리 페이지, 회원 상세보기 로직 구현

--- src/main/java/com/yhr/cleanCM/controller/AdmMemberController.java
+++ src/main/java/com/yhr/cleanCM/controller/AdmMemberController.java
@@ -1,11 +1,13 @@
 package com.yhr.cleanCM.controller;
 
+import com.yhr.cleanCM.dto.adm.AdmMemberDetailDTO;
 import com.yhr.cleanCM.dto.member.MemberDTO;
 import com.yhr.cleanCM.service.AdmMemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -25,6 +27,16 @@ public class AdmMemberController {
         model.addAttribute("members", members);
 
         return "adm/member/main";
+    }
+
+    @GetMapping("/members/detail/{id}")
+    public String showMemberDetail(@PathVariable(name = "id") Long id, Model model){
+
+        AdmMemberDetailDTO memberDetail = admMemberService.getMemberDetail(id);
+
+        model.addAttribute("member", memberDetail);
+
+        return "adm/member/detail";
     }
 
 }
--- src/main/java/com/yhr/cleanCM/service/AdmMemberService.java
+++ src/main/java/com/yhr/cleanCM/service/AdmMemberService.java
@@ -1,13 +1,17 @@
 package com.yhr.cleanCM.service;
 
 import com.yhr.cleanCM.dao.MemberRepository;
+import com.yhr.cleanCM.domain.Article;
 import com.yhr.cleanCM.domain.Member;
+import com.yhr.cleanCM.dto.adm.AdmMemberDetailDTO;
+import com.yhr.cleanCM.dto.article.ArticleListDTO;
 import com.yhr.cleanCM.dto.member.MemberDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -27,7 +31,32 @@ public class AdmMemberService {
         }
 
         return memberDTOList;
+    }
+
+    public AdmMemberDetailDTO getMemberDetail(Long id){
+
+        List<ArticleListDTO> articleDTOList = new ArrayList<>();
+
+        Optional<Member> memberOptional = memberRepository.findById(id);
+        
+        memberOptional.orElseThrow(
+                () -> new IllegalStateException("존재하지 않는 회원입니다.")
+        );
+
+        Member findMember = memberOptional.get();
+
+        List<Article> articles = findMember.getArticles();
+
+        for( Article article : articles ){
+
+            ArticleListDTO articleListDTO = new ArticleListDTO(article);
+            articleDTOList.add(articleListDTO);
+        }
+        return new AdmMemberDetailDTO(findMember, articleDTOList);
+
 
     }
+
+
 
 }
