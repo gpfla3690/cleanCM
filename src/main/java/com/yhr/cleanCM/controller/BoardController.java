434c6b48f2b59d842dfbd328b7d8e267c913c9b7
gpfla3690 <gpfla3690@gmail.com>
2022-02-21 15:31:27 +0900

    게시글 페이징 로직 구현

--- src/main/java/com/yhr/cleanCM/controller/BoardController.java
+++ src/main/java/com/yhr/cleanCM/controller/BoardController.java
@@ -2,6 +2,7 @@ package com.yhr.cleanCM.controller;
 
 import com.yhr.cleanCM.domain.Board;
 import com.yhr.cleanCM.domain.Member;
+import com.yhr.cleanCM.dto.article.ArticleListDTO;
 import com.yhr.cleanCM.dto.board.BoardDTO;
 import com.yhr.cleanCM.dto.board.BoardModifyForm;
 import com.yhr.cleanCM.dto.board.BoardSaveForm;
@@ -12,12 +13,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
+import java.util.Collections;
 import java.util.List;
 
 @Controller
@@ -40,13 +39,44 @@ public class BoardController {
 
     }
 
-    @GetMapping("/boards/{id}")
-    public String showBoardDetail(@PathVariable(name = "id") Long id, Model model) {
+    @GetMapping("/boards/{id}")  // http://localhost:8085/boards/1?page=1
+    public String showBoardDetail(@PathVariable(name = "id") Long id, Model model, @RequestParam(name="page", defaultValue = "1") int page) {
+
+        int size = 10;
 
         try {
 
             BoardDTO boardDetail = boardService.getBoardDetail(id);
+
+            List<ArticleListDTO> articleListDTO = boardDetail.getArticleListDTO();
+
+            Collections.reverse(articleListDTO);
+            // 0, 10, 20 ...
+            int startIndex = (page - 1) * size;
+            // 9, 19, 29 ... -> 15 -> 1.5(총 게시글 개수 / 10(size)) -> 올림
+            int lastIndex = ((page -1) * size) + 9;
+
+            int lastPage = (int)Math.ceil(articleListDTO.size()/(double)size);
+
+            if( page == lastPage ){  // ?page=2 == 2
+
+                lastIndex = articleListDTO.size(); // 15  [0,1,2,3,4,5,6,7,,,14]
+
+            }else if( page > lastPage ){  // ?page=100 == 2
+
+                return "redirect:/";
+
+            }else{  // ?page=1 == 2
+                lastIndex += 1;
+            }
+
+            // 페이지 자르기
+            List<ArticleListDTO> articlePage = articleListDTO.subList(startIndex, lastIndex); // [0, 10] -> 0,1,2,3,4,5,6,7,8,9
+
             model.addAttribute("board", boardDetail);
+            model.addAttribute("articles", articlePage);
+            model.addAttribute("maxPage", lastPage);
+            model.addAttribute("currentPage", page);
 
         } catch (Exception e) {
             return "redirect:/";
