4b7444aaaabcf48ebb455c481e61991266a6dd7f
gpfla3690 <gpfla3690@gmail.com>
2022-03-08 14:49:27 +0900

    마이페이지 ui 1차

--- src/main/java/com/yhr/cleanCM/service/MemberService.java
+++ src/main/java/com/yhr/cleanCM/service/MemberService.java
@@ -16,9 +16,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 
 @Service
@@ -109,6 +107,8 @@ public class MemberService implements UserDetailsService {
         Member findMember = findByLoginId(loginId);
 
         List<Article> articles = findMember.getArticles();
+
+        Collections.reverse(articles);
 
         for( Article article : articles ){
             ArticleDTO findArticle = articleService.getArticle(article.getId());
